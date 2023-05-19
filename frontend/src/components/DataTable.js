aad2ea5a2d1b841933fc4b486459b259be1961f6
Ignacio Bonelli <ignacio@nachodigital.com.ar>
2023-05-19 17:05:12 -0300

    Getting the information from the BE.

--- frontend/src/components/DataTable.js
+++ frontend/src/components/DataTable.js
@@ -23,25 +23,18 @@ export default function DataTable() {
 
   const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
-  ];
-  
+    { field: 'name', headerName: 'First name', width: 130 },
+    { field: 'lastname', headerName: 'Last name', width: 130 },
+    { field: 'country', headerName: 'Country', width: 130 },
+    { field: 'city', headerName: 'City', width: 130 },
+    { field: 'joinedDate', headerName: 'Joined', width: 90 },
+    { field: 'deliveryManager', headerName: 'DM', width: 90 },
+    { field: 'email', headerName: 'email', width: 90 },
+    { field: 'status', headerName: 'Status', width: 90 },
+    { field: 'exitDate', headerName: 'Left', width: 90 },
+];
+
+/*
   const rows = [
     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
@@ -53,11 +46,12 @@ export default function DataTable() {
     { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
   ];
-  
+*/
+
   return (
     <div style={{ height: 400, width: '100%' }}>
       <DataGrid
-        rows={rows}
+        rows={collaborators}
         columns={columns}
         initialState={{
           pagination: {
